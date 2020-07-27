6db9e0afc99acc8e2812d7c88a1eef5ae13fe9fd
Kirill Skvortsov <truemancheg@gmail.com>
2020-07-27 18:07:59 +0300

    init commit

---
+++ src/js/lib/core.js
@@ -0,0 +1,21 @@
+// (()=> {
+//     const $ = function(selector) {
+//         const elements = document.querySelectorAll(selector);
+//         const obj = {};
+//         obj.hide = () => {
+//             elements.forEach(item => {
+//                 item.style.display = "none";
+//             });
+//             return obj;
+//         };
+//         obj.show = () => {
+//             elements.forEach(item => {
+//                 item.style.display = "";
+//             });
+//             return obj;
+//         };
+//         return obj;
+//     };
+//     window.$ = $;
+// })();
+
