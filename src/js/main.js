15763c8a0a023f4bae6bc819806bc1d66fad7b0a
Kirill Skvortsov <truemancheg@gmail.com>
2020-07-27 20:43:15 +0300

    add work with css classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 import "./lib/core";
 import $ from "./lib/lib";
 
-$("div").toggle().show();
+$("div").addClass("hello", "world").removeClass("hello").toggleClass("active");
+console.log($("div").containClass("hello"));
+console.log($("div").containClass("world"));
+console.log($("div").containClass("third"));
