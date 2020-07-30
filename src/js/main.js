5d410f437c73537f1860bdf2411a627c2105b539
Kirill Skvortsov <truemancheg@gmail.com>
2020-07-30 21:52:56 +0300

    add dynamic modal creation

--- src/js/main.js
+++ src/js/main.js
@@ -13,5 +13,38 @@ $(".btn").eq(2).on("click", () => {
     $(".mt-20").fadeToggle(800);
 });
 
+$("#trigger").click(() => $("#trigger").createModal({
+    text: {
+        title: "Dynamic Modal Title",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                "Close",
+                ["btn-danger", "mr-10"],
+                true
+            ],
+            [
+                "Save Changes",
+                ["btn-success"],
+                false,
+                () => {
+                    alert("Data is saved");
+                }
+            ],
+            [
+                "New Button",
+                ["btn-warning", "ml-10"],
+                false,
+                () => {
+                    alert("New button is clicked");
+                }
+            ]
+        ]
+    }
+    
+}));
 
 
